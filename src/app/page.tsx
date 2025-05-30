aef4669813ed9d5d9d01fac9a26898636c838851
tyohs <gamhpzo@gmail.com>
2025-05-31 00:35:31 +0900

    '盤面作成完了、何の関数で管理すべきかが難しい'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,54 +2,134 @@
 
 import { useState } from 'react';
 import styles from './page.module.css';
-const calcTotalPoint = (arr: number[], counter: number) => {
-  counter;
-  const su = arr.reduce((accumulater, currntValue) => accumulater + currntValue);
-  return su + counter;
-};
-const down = (n: number) => {
-  if (n === -1) {
-    return;
-  } else {
-    console.log(n);
-    down(n - 1);
-  }
-};
-down(10);
-const sum1 = (n: number): number => {
-  return n <= 1 ? n : n + sum1(n - 1);
-};
-const sum2 = (s: number, i: number): number => {
-  // if (i <= s) {
-  //   return s;
-  // } else {
-  //   return i + sum2(s, i - 1);
-  // }
-  return i <= s ? s : i + sum2(s, i - 1);
-};
-const sum3 = (s: number, i: number): number => {
-  return (1 / 2) * (i - s + 1) * (s + i);
-};
-console.log(sum1(10));
-console.log(sum2(3, 10));
-console.log(sum3(4, 10));
+
+// const calcTotalPoint = (arr: number[], counter: number) => {
+//   counter;
+//   const su = arr.reduce((accumulater, currntValue) => accumulater + currntValue);
+//   return su + counter;
+// };
+// const down = (n: number) => {
+//   if (n === -1) {
+//     return;
+//   } else {
+//     console.log(n);
+//     down(n - 1);
+//   }
+// };
+// down(10);
+// const sum1 = (n: number): number => {
+//   return n <= 1 ? n : n + sum1(n - 1);
+// };
+// const sum2 = (s: number, i: number): number => {
+//   // if (i <= s) {
+//   //   return s;
+//   // } else {
+//   //   return i + sum2(s, i - 1);
+//   // }
+//   return i <= s ? s : i + sum2(s, i - 1);
+// };
+// const sum3 = (s: number, i: number): number => {
+//   return (1 / 2) * (i - s + 1) * (s + i);
+// };
+// console.log(sum1(10));
+// console.log(sum2(3, 10));
+// console.log(sum3(4, 10));
 export default function Home() {
   const [sampleCounter, setSampleCounter] = useState(0);
-  console.log('sampleCounter=', sampleCounter);
-  const [samplePoints, setSamplePoints] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  console.log('samplePoints=', samplePoints);
-  const clickHandler = () => {
-    setSampleCounter((sampleCounter + 1) % 14);
-    const newSamplePoints = structuredClone(samplePoints);
-    newSamplePoints[sampleCounter] += 1;
-    setSamplePoints(newSamplePoints);
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [userInputs, setUserInputs] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const bombMap = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  const directions = [
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+  ];
+  // console.log('sampleCounter=', sampleCounter);
+  // const [samplePoints, setSamplePoints] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  // console.log('samplePoints=', samplePoints);
+  // const totalPoint = calcTotalPoint(samplePoints, sampleCounter);
+  // console.log('totalPoint=', totalPoint);
+  let clickCount = 0;
+  const clickHandler = (x: number, y: number) => {
+    console.log(x, y);
+    const newUserInputs = structuredClone(userInputs);
+    newUserInputs[y][x] = 1;
+    setUserInputs(newUserInputs);
+    if (clickCount === 0) {
+      bombRandom();
+      clickCount += 1;
+    }
+    // setSampleCounter((sampleCounter + 1) % 14);
+    // const newSamplePoints = structuredClone(samplePoints);
+    // newSamplePoints[sampleCounter] += 1;
+    // setSamplePoints(newSamplePoints);
   };
-  const totalPoint = calcTotalPoint(samplePoints, sampleCounter);
-  console.log('totalPoint=', totalPoint);
+  const bombRandom = () => {
+    let bombCounter = 0;
+    while (bombCounter < 10) {
+      const y = Math.floor(Math.random() * 8);
+      const x = Math.floor(Math.random() * 8);
+      if (bombMap[y][x] === 0) {
+        bombMap[y][x] = 11;
+        bombCounter++;
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.sampleCell} style={{ backgroundPosition: sampleCounter * -30 }} />
-      <button onClick={clickHandler}>クリック</button>
+      {/* <div className={styles.sampleCell} style={{ backgroundPosition: sampleCounter * -30 }} /> */}
+      <div className={styles.flame}>
+        <div className={styles.board}>
+          {board.map((row, y) =>
+            row.map((value, x) => (
+              <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+                {userInputs[y][x] === 1 && (
+                  <div
+                    className={styles.sampleCell}
+                    style={{ backgroundPosition: sampleCounter * -30 }}
+                  />
+                )}
+              </div>
+            )),
+          )}
+        </div>
+      </div>
     </div>
   );
 }
